5ea32c6fd319f5481ced10a804a7f7260f174091
MatejDragicevic <matej.dragicevic.work@gmail.com>
2022-05-30 16:45:53 +0200

    Lecture 30

---
+++ src/datastructures/Cookie.java
@@ -0,0 +1,20 @@
+package datastructures;
+
+// class
+public class Cookie {
+    // class variable
+    private String color;
+
+    // constructor
+    public Cookie(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
--- src/datastructures/Main.java
+++ src/datastructures/Main.java
@@ -1,8 +1,8 @@
 package datastructures;
 
-public class Main {
+import java.util.*;
 
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
     }
 }
